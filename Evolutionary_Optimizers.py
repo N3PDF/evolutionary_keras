481e7ed4f5a8c652a2ca17d5e3fd57f68fd04b2d
juacrumar <juacrumar@gmail.com>
2020-01-17 11:04:59 +0100

    passed black to the files

--- Evolutionary_Optimizers.py
+++ Evolutionary_Optimizers.py
@@ -9,8 +9,8 @@ class EvolutionaryStragegies(Optimizer):
     """ 
     EvolutionaryStrategies contains functions shared by the Evolutionary Strategies in this file. 
     """
-    
-    #Can we remove this?
+
+    # Can we remove this?
     def get_updates(self, loss, params):
         pass
 
@@ -28,8 +28,10 @@ class NGA(EvolutionaryStragegies):
     adding normally distributed values with normal distrubtion given by sigma. 
     """
 
-    # In case the user wants to adjust sigma_original, population_size or mutation_rate parameters the NGA method has to be initiated 
-    def __init__(self, sigma_original = 15, population_size = 80, mutation_rate = 0.05, *args, **kwargs):
+    # In case the user wants to adjust sigma_original, population_size or mutation_rate parameters the NGA method has to be initiated
+    def __init__(
+        self, sigma_original=15, population_size=80, mutation_rate=0.05, *args, **kwargs
+    ):
         self.sigma_original = sigma_original
         self.population_size = population_size
         self.mutation_rate = mutation_rate
@@ -38,17 +40,15 @@ class NGA(EvolutionaryStragegies):
 
         super(NGA, self).__init__(*args, **kwargs)
 
-
     # Only works if all non_trainable_weights come after all trainable_weights
     # perhaps part of the functionality (getting shape) can be moved to ES
     def get_shape(self, model):
         original_weights = []
-        for i in range( len(model.trainable_weights) ):
+        for i in range(len(model.trainable_weights)):
             original_weights.append(model.get_weights()[i])
 
         self.non_training_weights = model.non_trainable_weights
 
-        
         self.N_nodes = 0
         weight_size = []
         for weight in original_weights:
@@ -58,7 +58,7 @@ class NGA(EvolutionaryStragegies):
                 shape = shape[0]
                 self.N_nodes += shape
         return weight_size
-        '''
+        """
 
 
         weight_size = []
@@ -72,83 +72,90 @@ class NGA(EvolutionaryStragegies):
                 c = len( original_weights[i] ) 
                 r = len( original_weights[i][0] )
                 weight_size.append( [ c, r ] )
-        return weight_size'''
-      
+        return weight_size"""
 
     # Takes a single mutant as input and creates a new generation by performing random nodal mutations
     # To change node and corresponding weights with probability, set change_both_weights_and_biases_of_a_node=True
-    # if change_both_weights_and_biases_of_a_node=False, either a node or the biases of a node will be mutated. 
-    def create_mutants(self, model, shape, change_both_weights_and_biases_of_a_node = True):
+    # if change_both_weights_and_biases_of_a_node=False, either a node or the biases of a node will be mutated.
+    def create_mutants(
+        self, model, shape, change_both_weights_and_biases_of_a_node=True
+    ):
         mutant = []
         mutant.append(model.get_weights())
         for k in range(self.population_size):
 
-            mutant.append(deepcopy( mutant[0] ) )
+            mutant.append(deepcopy(mutant[0]))
             mutated_nodes = []
 
             # select random nodes to mutate
-            for i in range( math.floor(self.N_nodes * self.mutation_rate ) ):
-                mutated_nodes.append( math.floor( self.N_nodes*np.random.rand() ) )
+            for i in range(math.floor(self.N_nodes * self.mutation_rate)):
+                mutated_nodes.append(math.floor(self.N_nodes * np.random.rand()))
 
             for i in mutated_nodes:
 
                 # find the nodes in their respective layers
                 count_nodes = 0
                 layer = 1
-                # HERE WEIGHT-BIAS-WEIGHT-BIAS IS ALSO ASSUMED BY THE +=2 
+                # HERE WEIGHT-BIAS-WEIGHT-BIAS IS ALSO ASSUMED BY THE +=2
                 while count_nodes <= i:
-                    count_nodes += shape[layer][0] 
+                    count_nodes += shape[layer][0]
                     layer += 2
                 layer -= 2
                 count_nodes -= shape[layer][0]
-                node_in_layer = i - count_nodes 
+                node_in_layer = i - count_nodes
 
                 # mutate weights and biases by adding values from random distrubution
-                sigma_eff = self.sigma * ( self.N_generations ** ( -np.random.rand() ) )
+                sigma_eff = self.sigma * (self.N_generations ** (-np.random.rand()))
                 if change_both_weights_and_biases_of_a_node:
-                    randn_mutation = sigma_eff * np.random.randn( shape[layer-1][0] ) 
-                    mutant[k+1][layer-1][:,node_in_layer] += randn_mutation
-                    mutant[k+1][layer][node_in_layer] += sigma_eff * np.random.randn() 
+                    randn_mutation = sigma_eff * np.random.randn(shape[layer - 1][0])
+                    mutant[k + 1][layer - 1][:, node_in_layer] += randn_mutation
+                    mutant[k + 1][layer][node_in_layer] += sigma_eff * np.random.randn()
                 else:
-                    bias_or_weights = math.floor(2 * np.random.rand() )
+                    bias_or_weights = math.floor(2 * np.random.rand())
                     if bias_or_weights == 0:
-                        randn_mutation = sigma_eff * np.random.randn( shape[layer-1][0] ) 
-                        mutant[k+1][layer-1][:,node_in_layer] += randn_mutation
+                        randn_mutation = sigma_eff * np.random.randn(
+                            shape[layer - 1][0]
+                        )
+                        mutant[k + 1][layer - 1][:, node_in_layer] += randn_mutation
                     else:
-                        mutant[k+1][layer][node_in_layer] += sigma_eff * np.random.randn() 
-                
+                        mutant[k + 1][layer][node_in_layer] += (
+                            sigma_eff * np.random.randn()
+                        )
+
                 # add non_training weights such that 'mutant' now parametrizes the entire model
-                mutant[k+1].append(self.non_training_weights)
-                
+                mutant[k + 1].append(self.non_training_weights)
+
         return mutant
 
     # RESULTS DO IMPROVE WHEN TRAINING BASED ON SELECTING HIGHEST ACCURACY, BUT NOT IF SELECTION IS BASED ON LOWEST LOSS
     # ALSO, THE ACCURACY CHANGES AFTER THE WEIGHTS OF THE BEST PERFROMING MODEL ARE LOADED INTO THE MODEL AGAIN, IS THERE
     # SOME RANDOMIZATION DONE BY TENSORFLOW? BUT THE MODEL DOES NOT INCLUDE THINGS SUCH AS DROPOUT LAYER. SHOULD WE USE DIFFERENT
     # METHOD TO SAVE AND LOAD MODELS?
-     
-    # Evalutates all mutantants of a generationa and ouptus loss and the single best performing mutant of the generation 
-    def evaluate_mutants(self, model, mutant, x=None, y=None, ):
+
+    # Evalutates all mutantants of a generationa and ouptus loss and the single best performing mutant of the generation
+    def evaluate_mutants(
+        self, model, mutant, x=None, y=None,
+    ):
 
         # most_accurate_model=0 corresponds to setting the input model as most accurate
-        most_accurate_model = 0   
-        for i in range(self.population_size+1):
-                # replace weights of the input model by weights generated using the create_mutants function
-                model.set_weights(mutant[i])
-                output=model.evaluate(verbose=False, x=x, y=y)
-                if self.loss_is_list_type:
-                    loss_new = output[0]
-                else:
-                    loss_new = output
+        most_accurate_model = 0
+        for i in range(self.population_size + 1):
+            # replace weights of the input model by weights generated using the create_mutants function
+            model.set_weights(mutant[i])
+            output = model.evaluate(verbose=False, x=x, y=y)
+            if self.loss_is_list_type:
+                loss_new = output[0]
+            else:
+                loss_new = output
 
-                if(loss_new < self.loss):
-                    self.loss = loss_new
-                    most_accurate_model = i
+            if loss_new < self.loss:
+                self.loss = loss_new
+                most_accurate_model = i
 
         # if none of the mutants have performed better on the training data than the original mutant, reduce sigma
-        if(most_accurate_model == 0):
+        if most_accurate_model == 0:
             self.N_generations += 1
-            self.sigma = self.sigma_original/self.N_generations
+            self.sigma = self.sigma_original / self.N_generations
 
         return output, mutant[most_accurate_model]
 
@@ -156,26 +163,26 @@ class NGA(EvolutionaryStragegies):
 
     # Run during compilation to get objects that are unchanged at fit()
     def prepare_during_compile(self, model):
-       self.shape = self.get_shape( model=model ) 
-
+        self.shape = self.get_shape(model=model)
 
     # Collects the functions defined above to form the fitting step that is to be repeated for a number of epochs
     def run_step(self, model, x, y):
-        
+
         # Initialize training paramters
         if self.has_init_variables != True:
             self.N_generations = 1
             self.loss = model.evaluate(x=x, y=y, verbose=0)
-            if isinstance( self.loss, list): 
+            if isinstance(self.loss, list):
                 self.loss = self.loss[0]
                 self.loss_is_list_type = True
-            self.has_init_variables = True     
+            self.has_init_variables = True
 
-        mutant = self.create_mutants( model=model, shape=self.shape )
-        score, selected_parent = self.evaluate_mutants(model=model, mutant=mutant, x=x, y=y)
+        mutant = self.create_mutants(model=model, shape=self.shape)
+        score, selected_parent = self.evaluate_mutants(
+            model=model, mutant=mutant, x=x, y=y
+        )
 
         return score, selected_parent
-
 
 
 class CMA(EvolutionaryStragegies):
--- GAModel.py
+++ GAModel.py
@@ -1,35 +1,34 @@
-from keras.models import Model 
+from keras.models import Model
 import keras.optimizers as keras_opt
-import Evolutionary_Optimizers 
+import Evolutionary_Optimizers
 import numpy as np
 
 
-# Dictionary of the new evolutionay optimizers 
-optimizers={
-    "ga" : Evolutionary_Optimizers.GA,
-    "nga" : Evolutionary_Optimizers.NGA,
-    "cma" : Evolutionary_Optimizers.CMA,
-    "bfgs" : Evolutionary_Optimizers.BFGS,
-    "ceressolver" : Evolutionary_Optimizers.CeresSolver
+# Dictionary of the new evolutionay optimizers
+optimizers = {
+    "ga": Evolutionary_Optimizers.GA,
+    "nga": Evolutionary_Optimizers.NGA,
+    "cma": Evolutionary_Optimizers.CMA,
+    "bfgs": Evolutionary_Optimizers.BFGS,
+    "ceressolver": Evolutionary_Optimizers.CeresSolver,
 }
 
 
 class GAModel(Model):
-
 
     """  
     GAModel forewards all tasks to keras if the optimizer is NOT genetic. In case the optimizer is genetic, fitting methods 
     from Evolutionary_Optimizers.py are being used. 
     """
 
-
-    # Initialization is not being at this point and hence superfluous 
+    # Initialization is not being at this point and hence superfluous
     def __init__(self, input_tensor, output_tensor, **kwargs):
         super(GAModel, self).__init__(input_tensor, output_tensor, **kwargs)
 
-    
-    def compile(self, optimizer, **kwargs): # can we remove anything but optimizer from these inputs?
-        
+    def compile(
+        self, optimizer, **kwargs
+    ):  # can we remove anything but optimizer from these inputs?
+
         # Checks wether the optimizer is genetic or not and creates an optimizer instance in case a string type was given as input
         self.is_genetic = False
         # Checks (if the optimizer input is a string) whether it is in the 'optimizers' dictionary
@@ -38,7 +37,7 @@ class GAModel(Model):
             if optimizer in optimizers.keys():
                 optimize = optimizers[optimizer]()
                 self.is_genetic = True
-        # Checks if the optimizer is an evolutionary strategy    
+        # Checks if the optimizer is an evolutionary strategy
         elif isinstance(optimizer, Evolutionary_Optimizers.EvolutionaryStragegies):
             optimize = optimizer
             self.is_genetic = True
@@ -46,14 +45,13 @@ class GAModel(Model):
 
         # If the optimizer is genetic, compile using keras while setting a random (keras supported) gradient descent optimizer
         if self.is_genetic:
-            super().compile(optimizer='rmsprop', **kwargs)
-            self.optimizer_instance.prepare_during_compile(model = self)
-        else: 
+            super().compile(optimizer="rmsprop", **kwargs)
+            self.optimizer_instance.prepare_during_compile(model=self)
+        else:
             super().compile(optimizer=optimizer, **kwargs)
-    
-   
+
     # If the optimizer is genetic the fitting precedure consists of executing run_stop for the given number of epochs
-    def fit(self, x=None, y=None, validation_data=None, epochs=1, verbose = 0, **kwargs):
+    def fit(self, x=None, y=None, validation_data=None, epochs=1, verbose=0, **kwargs):
         if self.is_genetic:
             # Validation data is currently not being used!!
             if validation_data is not None:
@@ -61,31 +59,48 @@ class GAModel(Model):
                 y_val = validation_data[1]
 
             for epoch in range(epochs):
-                score, best_mutant = self.optimizer_instance.run_step( model = self, x=x, y=y )
+                score, best_mutant = self.optimizer_instance.run_step(
+                    model=self, x=x, y=y
+                )
                 self.set_weights(best_mutant)
 
                 if epoch == 0:
-                    # use numpy array becuase list can only give one type of score at each epoch 
+                    # use numpy array becuase list can only give one type of score at each epoch
                     # (no different values in the same row of the list (probably just something I don't know how) )
-                    history_temp = np.zeros(( len(score), epochs ))
+                    history_temp = np.zeros((len(score), epochs))
                 if verbose == 1:
-                    print('epoch: ', epoch+1, '/', epochs, ', train_accuracy: ', score[1], 'sigma:', self.optimizer_instance.sigma ) 
-                
-                for i in range( len(score) ):
+                    print(
+                        "epoch: ",
+                        epoch + 1,
+                        "/",
+                        epochs,
+                        ", train_accuracy: ",
+                        score[1],
+                        "sigma:",
+                        self.optimizer_instance.sigma,
+                    )
+
+                for i in range(len(score)):
                     history_temp[i][epoch] = score[i]
 
             # keras fit outputs history as dict with dict.values list type, so we convert numpy->list as well
             history_temp = history_temp.tolist()
             history = {}
-            for i in range ( len(score) ):
-                history[ self.metrics_names[i] ] = history_temp[i] 
-            
-            return returnvalues(self, history, epochs, validation_data)
-          
-        else: 
-            # if not is_gentic, let keras deal with the fit.
-            return super().fit(x=x, y=y, validation_data=validation_data, epochs=epochs, verbose=verbose, **kwargs)
+            for i in range(len(score)):
+                history[self.metrics_names[i]] = history_temp[i]
 
+            return returnvalues(self, history, epochs, validation_data)
+
+        else:
+            # if not is_gentic, let keras deal with the fit.
+            return super().fit(
+                x=x,
+                y=y,
+                validation_data=validation_data,
+                epochs=epochs,
+                verbose=verbose,
+                **kwargs
+            )
 
     # Evaluate is done by keras
     def evaluate(self, *args, **kwargs):
@@ -93,7 +108,9 @@ class GAModel(Model):
 
 
 class returnvalues:
-    def __init__(self, model=None, history=None, epochs=None, validation_data=None, params=None):
+    def __init__(
+        self, model=None, history=None, epochs=None, validation_data=None, params=None
+    ):
         self.history = history
         self.epoch = [*range(epochs)]
         self.model = model
