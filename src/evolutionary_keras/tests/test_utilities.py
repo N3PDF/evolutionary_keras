5ed2eecc88bd7da26486ace6ec56d6dbea347171
Roy Stegeman <roystegeman@live.nl>
2020-04-23 14:15:09 +0200

    make test_utities.py tf2.2 compatible

--- src/evolutionary_keras/tests/test_utilities.py
+++ src/evolutionary_keras/tests/test_utilities.py
@@ -1,10 +1,11 @@
 """ Test the utilities of evolutionary_keras """
 
 import numpy as np
-from keras import backend as K
-from keras.layers import Dense
+from tensorflow.keras import backend as K
+from tensorflow.keras.layers import Dense, Input
 
 from evolutionary_keras.utilities import get_number_nodes, parse_eval
+from evolutionary_keras.models import EvolModel
 
 
 def test_parse_eval():
@@ -19,10 +20,13 @@ def test_parse_eval():
 
 def test_get_number_nodes():
     """ Get the number of nodes of a layer """
-    ii = K.constant(np.random.rand(2, 1))
+
     nodes = 10
-    layer = Dense(units=nodes)
-    # Keras won't build the layer until it is called with some input
-    _ = layer(ii)
+
+    # Tensorflow won't build the layer until it is called in a model
+    input_layer = Input(shape=(1,))
+    output_layer = Dense(units=nodes, name="test_layer")
+    modelito = EvolModel(input_layer, output_layer(input_layer))
+    
     # Check that indeed the number of nodes is parsed correctly
-    assert nodes == get_number_nodes(layer)
+    assert nodes == get_number_nodes(output_layer)
