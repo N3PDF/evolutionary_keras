91806b8d5ba7957d9fa13d9770efff22f5980b6b
juacrumar <juacrumar@gmail.com>
2020-04-23 12:14:24 +0200

    workaround for 2.2

--- src/evolutionary_keras/models.py
+++ src/evolutionary_keras/models.py
@@ -27,7 +27,7 @@ class EvolModel(Model):
         super().__init__(*args, **kwargs)
         self.is_genetic = False
         self.opt_instance = None
-        self.history = History()
+        self.history_info = History()
 
     def parse_optimizer(self, optimizer):
         """ Checks whether the optimizer is genetic and creates and optimizer instance in case a
@@ -51,6 +51,7 @@ class EvolModel(Model):
         """ When the optimizer is genetic, compiles the model in keras setting an arbitrary
         keras supported optimizer """
         self.parse_optimizer(optimizer)
+        self.history_info.set_model(self)
         if self.is_genetic:
             super().compile(optimizer="rmsprop", **kwargs)
         else:
@@ -72,7 +73,7 @@ class EvolModel(Model):
                 verbose, prints to log.info the loss per epoch
         """
         # Prepare the history for the initial epoch
-        self.history.on_train_begin()
+        self.history_info.on_train_begin()
         # Validation data is currently not being used!!
         if validation_data is not None:
             log.warning(
@@ -106,8 +107,8 @@ class EvolModel(Model):
                     history_data = dict(zip(metricas, score))
                 else:
                     raise e
-            self.history.on_epoch_end(epoch, history_data)
-        return self.history
+            self.history_info.on_epoch_end(epoch, history_data)
+        return self.history_info
 
     def fit(self, x=None, y=None, validation_data=None, epochs=1, verbose=0, **kwargs):
         """ If the optimizer is genetic, the fitting procedure consists on executing `run_step` for
