2805fe23518f546e057ba4cb78f7179b2d7c8415
RoyStegeman <roystegeman@live.nl>
2020-08-17 11:23:26 +0200

    fix odd/even layer shape check

--- src/evolutionary_keras/optimizers.py
+++ src/evolutionary_keras/optimizers.py
@@ -111,15 +111,16 @@ class NGA(EvolutionaryStrategies):
         self.n_nodes = get_number_nodes(self.model)
 
         # check compatibility of the shape with the NGA optimizer
+        NNshape = None
+        count_nodes = 0
         for num, layer_shape in enumerate(weight_shapes):
-            count_nodes = 0
             num += 1
-            if num % 1 == 0:
-                if np.array(layer_shape).size != 2:
-                    NNshape = "incompatible"
             if num % 2 == 0:
-                count_nodes += np.array(layer_shape).size
+                count_nodes += np.array(layer_shape)
                 if np.array(layer_shape).size != 1:
+                    NNshape = "incompatible"
+            else:
+                if np.array(layer_shape).size != 2:
                     NNshape = "incompatible"
         if count_nodes != self.n_nodes:
             NNshape = "incompatible"
@@ -127,7 +128,6 @@ class NGA(EvolutionaryStrategies):
             raise ValueError(
                 "Using the NGA optimizer requires the network to have a (weight-bias)^n structure, with 1-dimensional bias"
             )
-
         return weight_shapes
 
     def create_mutants(self, change_both_wb=True):
